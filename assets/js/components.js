2c6672c628156c1764f96e946e71643f0b3b0f40
Paulo Silva <paulofernando78@gmail.com>
2025-03-20 01:34:32 -0300

    wip

--- assets/js/components.js
+++ assets/js/components.js
@@ -2,13 +2,13 @@
 import { WcButton } from "/src/components/atoms/Button.js";
 import { WcIconList } from "/src/components/molecules/IconList.js";
 
-// Função para registrar Web Components sem repetir
+// Function to register Web Components without repeating
 const registerComponent = (name, component) => {
   if (!customElements.get(name)) {
     customElements.define(name, component);
   }
 };
 
-// Registrar os componentes
+// Register components
 registerComponent("wc-button", WcButton);
 registerComponent("wc-icon-list", WcIconList);
--- src/components/molecules/IconList.js
+++ src/components/molecules/IconList.js
@@ -1,21 +1,56 @@
-
 export class WcIconList extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    
+    this.items = [];
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ["items"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "items" && newValue !== oldValue) {
+      try {
+        this.items = JSON.parse(newValue);
+        this.render();
+      } catch (e) {
+        console.error("Failed to parse items attribute:", e);
+      }
+    }
+  }
+
+  render() {
     this.shadowRoot.innerHTML = `
       <style>
-        img {
-          margin-right: .625rem;
+        ul, li {
+          margin: 0;
+          padding: 0;
+        }
+        li {
+          list-style-type: none;
+          font-size: var(--p-formatting);
+          line-height: var(--p-line-height);
         }
       </style>
       <ul>
-        <li>
-        ${svg}>${item}
-        <p>teste</p>
-        </li>
+        ${this.items.map(
+          (item) => `
+          <li>
+            ${item.list || ""}
+          </li>
+        `
+        ).join('')}
       </ul>
     `;
   }
+
+  // Helper method to set items programmatically
+  setItems(items) {
+    this.items = items;
+    this.render();
+  }
 }
+
+customElements.define("wc-icon-list", WcIconList);
